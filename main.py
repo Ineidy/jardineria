bbf259db3928381d12459872be68b084219ca4b8
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-03-12 16:10:59 -0500

    filtros y mas filtros

--- main.py
+++ main.py
@@ -8,33 +8,37 @@ import module.getpago as pa
 import module.getPedidos as pedi
 
 if (__name__ == '__main__'):
-    print("""
-    __  ___                    ____       _            _             __
-   /  |/  /__  ____  __  __   / __ \_____(_)___  _____(_)___  ____ _/ /
-  / /|_/ / _ \/ __ \/ / / /  / /_/ / ___/ / __ \/ ___/ / __ \/ __ `/ / 
- / /  / /  __/ / / / /_/ /  / ____/ /  / / / / / /__/ / /_/ / /_/ / /  
-/_/  /_/\___/_/ /_/\__,_/  /_/   /_/  /_/_/ /_/\___/_/ .___/\__,_/_/   
-                                                    /_/               
+    while True:
+        print("""
+        __  ___                    ____       _            _             __
+       /  |/  /__  ____  __  __   / __ \_____(_)___  _____(_)___  ____ _/ /
+      / /|_/ / _ \/ __ \/ / / /  / /_/ / ___/ / __ \/ ___/ / __ \/ __ `/ / 
+     / /  / /  __/ / / / /_/ /  / ____/ /  / / / / / /__/ / /_/ / /_/ / /  
+    /_/  /_/\___/_/ /_/\__,_/  /_/   /_/  /_/_/ /_/\___/_/ .___/\__,_/_/   
+                                                        /_/               
 
 
-                            1. cliente
-                            2. oficina
-                            3. empleados
-                            4. pedidos 
-                            5. pagos
-                            6. productos
-""")
-    
-    opcion = int(input("\n Seleccione una de las opciones: "))
-    if(opcion == 1):
-        cli.menu()
-    elif(opcion == 2):
-        of.menu()
-    elif(opcion == 3):
-        empleado.menu()
-    elif(opcion == 4):
-        pedi.menu()
-    elif(opcion == 5):
-        pa.menu()
-    elif(opcion == 6):
-        produ.menu()
+                                1. cliente
+                                2. oficina
+                                3. empleados
+                                4. pedidos 
+                                5. pagos
+                                6. productos
+                                0. salir
+    """)
+        
+        opcion = int(input("\n Seleccione una de las opciones: "))
+        if(opcion == 1):
+            cli.menu()
+        elif(opcion == 2):
+            of.menu()
+        elif(opcion == 3):
+            empleado.menu()
+        elif(opcion == 4):
+            pedi.menu()
+        elif(opcion == 5):
+            pa.menu()
+        elif(opcion == 6):
+            produ.menu()
+        elif(opcion==0):
+            break
--- module/getClients.py
+++ module/getClients.py
@@ -1,4 +1,5 @@
 import storage.cliente as cli
+import storage.empleado as empleado
 from tabulate import tabulate
 
 def getAllClientesName():
@@ -67,50 +68,107 @@ def getAllNombrePais(pais):
     return nombrePais
 
 
+#devuelve un listado con todos los clientes que sean de la ciudad de Madrid 
+#y cuyo representante de ventas tenga el codigo de empleado 11 o 30.
+def getAllClientsMadridRepVentas30o11():
+    clientsMadrid = []
+    for val in cli.clientes:
+        if( 
+            val.get('ciudad') == 'Madrid' and 
+            (val.get('codigo_empleado_rep_ventas') == 11 or val.get('codigo_empleado_rep_ventas') == 30)
+
+        ):
+            clientsMadrid.append(
+                {
+                    "Codigo_Del_Cliente": val.get('codigo_cliente'),
+                    "Nombre_Del_Cliente": val.get('nombre_cliente'),
+                    "Codigo_De_Empleado": val.get('codigo_empleado_rep_ventas'),
+                    "Ciudad": val.get('ciudad')
+                }
+            )
+    return clientsMadrid
+
+
+
+
+#obtener un listado con el nombre de cada cliente y el nombre y apellido del representante de ventas
+def getAllClientesReprVentas():
+    clientsReprVentas = []
+    for val in cli.clientes:
+        for i in empleado.empleado:
+            if val.get("codigo_empleado_rep_ventas") == i.get("codigo_empleado"):
+                clientsReprVentas.append(
+                    {
+                     "Nombre":val.get('nombre_cliente'),
+                     "Apellidos": F'{val.get("apellido1")}{val.get("apellido2")}'
+                    }
+                )
+        return clientsReprVentas
+
+        
+        
+
+
 
 def menu():
-    print("""
-          
+    while True:
+        print("""
+            
 
-   ___  _______  ____  ___  ______________  ___  ____  _______   _________  ________________
-  / _ \/ __/ _ \/ __ \/ _ \/_  __/ __/ __/ / _ \/ __/ / ___/ /  /  _/ __/ |/ /_  __/ __/ __/
- / , _/ _// ___/ /_/ / , _/ / / / _/_\ \  / // / _/  / /__/ /___/ // _//    / / / / _/_\ \  
-/_/|_/___/_/   \____/_/|_| /_/ /___/___/ /____/___/  \___/____/___/___/_/|_/ /_/ /___/___/  
-                                                                                            
+       ___  _______  ____  ___  ______________  ___  ____  _______   _________  ________________
+      / _ \/ __/ _ \/ __ \/ _ \/_  __/ __/ __/ / _ \/ __/ / ___/ /  /  _/ __/ |/ /_  __/ __/ __/
+     / , _/ _// ___/ /_/ / , _/ / / / _/_\ \  / // / _/  / /__/ /___/ // _//    / / / / _/_\ \  
+    /_/|_/___/_/   \____/_/|_| /_/ /___/___/ /____/___/  \___/____/___/___/_/|_/ /_/ /___/___/  
+                                                                                                
 
-          1. obtener todos los clientes (nombre).
+            1. obtener todos los clientes (nombre).
 
-          2. obtener un cliente por el codigo.
+            2. obtener un cliente por el codigo.
 
-          3. obtener toda la informacion de un cliente segun su limite de credito
-             y ciudad que pertenece (ejem: 3000, San Francisco).
-          
-          4. obtener clientes por pais, region y ciudad.
+            3. obtener toda la informacion de un cliente segun su limite de credito
+                y ciudad que pertenece (ejem: 3000, San Francisco).
+            
+            4. obtener clientes por pais, region y ciudad.
 
-          5. obtener cliente por pais.
+            5. obtener cliente por pais.
+
+            6. obtener listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el codigo de empleado 11 o 30.
+              
+            7. obtener cada cielte con su representante de ventas
+              
+            0. Salir
 
 
-""")
-    
-    opcion = int(input("\nElija una de las opciones: "))
-    if(opcion == 1):
-        print(tabulate(getAllClientesName(), headers="keys", tablefmt = 'rounded_grid'))
-    
-    elif(opcion == 2):
-        codigoCliente = int(input("Ingrese el codigo del cliente: "))
-        print(tabulate(getOneClientCodigo(codigoCliente), headers="keys",tablefmt = 'rounded_grid'))
-    
-    elif(opcion == 3):
-        limite = int(input("Ingrese el limite de credito de los clientes que deseas visualizar: "))
-        ciudad = input("ingrese la ciudad que desea filtrar los clientes: ")
-        print(tabulate(getAllClientCreditCiudad(limite, ciudad), headers="keys", tablefmt='rounded_grid'))
-     
-    elif(opcion == 4):
-        pais = input("Ingrese el pais que desea filtrar: ")
-        region = input("Ingrese la region que desea filtrar: ")
-        ciudad = input("Ingrese la ciudad que desea filtrar: ")
-        print(tabulate(getAllClientPaisRegionCiudad(pais, region, ciudad), headers="keys", tablefmt='rounded_grid'))
+    """)
+        
+        opcion = int(input("\nElija una de las opciones: "))
+        if(opcion == 1):
+            print(tabulate(getAllClientesName(), headers="keys", tablefmt = 'rounded_grid'))
+        
+        elif(opcion == 2):
+            codigo = int(input("Ingrese el codigo del cliente: "))
+            print(tabulate(getOneClientCodigo(codigo), headers="keys",tablefmt = 'rounded_grid'))
+        
+        elif(opcion == 3):
+            limite = int(input("Ingrese el limite de credito de los clientes que deseas visualizar: "))
+            ciudad = input("ingrese la ciudad que desea filtrar los clientes: ")
+            print(tabulate(getAllClientCreditCiudad(limite, ciudad), headers="keys", tablefmt='rounded_grid'))
+        
+        elif(opcion == 4):
+            pais = input("Ingrese el pais que desea filtrar: ")
+            region = input("Ingrese la region que desea filtrar: ")
+            ciudad = input("Ingrese la ciudad que desea filtrar: ")
+            print(tabulate(getAllClientPaisRegionCiudad(pais, region, ciudad), headers="keys", tablefmt='rounded_grid'))
 
-    elif(opcion == 5):
-        pais = input("Ingrese el pais del que quiere filtrar el cliente: ")
-        print(tabulate(getAllNombrePais(pais), headers="keys", tablefmt="rounded_grid"))
+        elif(opcion == 5):
+            pais = input("Ingrese el pais del que quiere filtrar el cliente: ")
+            print(tabulate(getAllNombrePais(pais), headers="keys", tablefmt="rounded_grid"))
+
+        elif(opcion==6):
+            print(tabulate(getAllClientsMadridRepVentas30o11(), headers="keys", tablefmt='rounded_grid'))
+
+        elif(opcion==7):
+            print(tabulate(getAllClientesReprVentas(),headers="keys", tablefmt='rounded_grid'))
+
+        elif(opcion==0):
+            break
